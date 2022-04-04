204f0511e20ac427c82409e553444b7e435ebda1
Mohamed Alaa Eddin <dodo1003pro@gmail.com>
2022-04-04 08:48:55 +0200

    Modified Functions Names & Added RGB READ and Write Functions

--- main.cpp
+++ main.cpp
@@ -17,8 +17,18 @@ using namespace std;
 unsigned char image[SIZE][SIZE];
 unsigned char image1[SIZE][SIZE];
 unsigned char newImage[SIZE][SIZE];
+unsigned char RGBImage[SIZE][SIZE][RGB];
+unsigned char newRGBImage[SIZE][SIZE][RGB];
 
-void saveImage() {
+void readGSImage() {
+    char imageFileName[100];
+    cout << "Enter The Source Image File Name:";
+    cin >> imageFileName;
+    strcat(imageFileName, ".bmp");
+    readGSBMP(imageFileName, image);
+}
+
+void saveGSImage() {
     char imageFileName[100];
 
     // Get gray scale image target file name
@@ -29,10 +39,11 @@ void saveImage() {
     strcat(imageFileName, ".bmp");
     writeGSBMP(imageFileName, newImage);
 }
+//------------------------------GREY SCALE BITMAP FILTERS------------------------------//
 
 // BLACK AND WHITE Image #1
-void blackAndWhite() {
-
+void GSblackAndWhite() {
+    readGSImage();
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             if (image[i][j] > 127)
@@ -41,21 +52,23 @@ void blackAndWhite() {
                 newImage[i][j] = 0;
         }
     }
-    saveImage();
+    saveGSImage();
 }
 
 // INVERT Image #2
-void invertImage() {
+void GSinvertImage() {
+    readGSImage();
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             newImage[i][j] = 255 - image[i][j];
         }
     }
-    saveImage();
+    saveGSImage();
 }
 
 // Merge Image #3
-void merge() {
+void GSmerge() {
+    readGSImage();
     char image2ndFileName[100];
     cout << "Enter The Second Source Image File Name:";
     cin >> image2ndFileName;
@@ -66,11 +79,12 @@ void merge() {
             newImage[i][j] = (image1[i][j] + image[i][j]) / 2;
         }
     }
-    saveImage();
+    saveGSImage();
 }
 
 // FLIP Image #4
-void flip() {
+void GSflip() {
+    readGSImage();
     int n;
     cout << "\n1.Horizontally\n"
             "2.Vertically\n"
@@ -82,7 +96,7 @@ void flip() {
                 newImage[i][j] = image[255 - i][j];
             }
         }
-        saveImage();
+        saveGSImage();
     }
     if (n == 2) {
         for (int i = 0; i < SIZE; i++) {
@@ -90,32 +104,35 @@ void flip() {
                 newImage[i][j] = image[i][255 - j];
             }
         }
-        saveImage();
+        saveGSImage();
     }
 
 
 }
 
 // ROTATE Image #5
-void rotate90() {
+void GSrotate90() {
+    readGSImage();
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             newImage[i][j] = image[255 - j][i];
         }
     }
-    saveImage();
+    saveGSImage();
 }
 
-void rotate180() {
+void GSrotate180() {
+    readGSImage();
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             newImage[i][j] = image[255 - i][255 - j];
         }
     }
-    saveImage();
+    saveGSImage();
 }
 
-void rotate270() {
+void GSrotate270() {
+    readGSImage();
     int k = 255;
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -123,11 +140,12 @@ void rotate270() {
         }
         k--;
     }
-    saveImage();
+    saveGSImage();
 }
 
 // Darken And Lighten Image #6
-void darkenAndLighten() {
+void GSdarkenAndLighten() {
+    readGSImage();
     int n;
     cout << "\n1.Darken"
             "\n2.Lighten"
@@ -142,7 +160,7 @@ void darkenAndLighten() {
                     newImage[i][j] = image[i][j] + 128;
             }
         }
-        saveImage();
+        saveGSImage();
     }
 
     if (n == 1) {
@@ -151,12 +169,13 @@ void darkenAndLighten() {
                 newImage[i][j] = image[i][j] * 0.5;
             }
         }
-        saveImage();
+        saveGSImage();
     }
 }
 
 // Detect Images Edges #7
-void detectEdges() {
+void GSdetectEdges() {
+    readGSImage();
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             if (image[i][j] > 127)
@@ -174,10 +193,12 @@ void detectEdges() {
                 newImage[i][j] = image1[i][j];
         }
     }
-    saveImage();
+    saveGSImage();
 }
+
 // Enlarge Image #8
-void enlarge() {
+void GSenlarge() {
+    readGSImage();
     int quarter;
     cout << "Enter Quarter:";
     cin >> quarter;
@@ -190,7 +211,7 @@ void enlarge() {
                 newImage[(i * 2) + 1][(j * 2) + 1] = image[i][j];
             }
         }
-        saveImage();
+        saveGSImage();
     } else if (quarter == 2) {
         for (int i = 0; i < SIZE / 2; i++) {
             for (int j = 127; j < SIZE; j++) {
@@ -200,7 +221,7 @@ void enlarge() {
                 newImage[(i * 2) + 1][(j * 2) + 1] = image[i][j];
             }
         }
-        saveImage();
+        saveGSImage();
     } else if (quarter == 3) {
         for (int i = SIZE / 2; i < SIZE; i++) {
             for (int j = 0; j < SIZE / 2; j++) {
@@ -210,7 +231,7 @@ void enlarge() {
                 newImage[((i - SIZE / 2) * 2) + 1][(j * 2) + 1] = image[i][j];
             }
         }
-        saveImage();
+        saveGSImage();
     } else if (quarter == 4) {
         for (int i = SIZE / 2; i < SIZE; i++) {
             for (int j = SIZE / 2; j < SIZE; j++) {
@@ -220,12 +241,13 @@ void enlarge() {
                 newImage[((i - SIZE / 2) * 2) + 1][(j * 2) + 1] = image[i][j];
             }
         }
-        saveImage();
+        saveGSImage();
     }
 }
 
 // SHRINK Image #9
-void shrink() {
+void GSshrink() {
+    readGSImage();
     int row = 0;
     int operation;
     cout << "\n1.Shrink to 1/2"
@@ -252,7 +274,7 @@ void shrink() {
             }
             row += 3;
         }
-        saveImage();
+        saveGSImage();
     }
     if (operation == 3) {
         for (int i = 0; i < SIZE / 4; i++) {
@@ -263,12 +285,13 @@ void shrink() {
             }
             row += 4;
         }
-        saveImage();
+        saveGSImage();
     }
 }
 
 // Mirror Image #A
-void mirror() {
+void GSmirror() {
+    readGSImage();
     int n;
     cout << "\n1.Left 1/2"
             "\n2.Right 1/2"
@@ -287,7 +310,7 @@ void mirror() {
                 newImage[i][j] = image[i][255 - j];
             }
         }
-        saveImage();
+        saveGSImage();
     }
     if (n == 2) {
         for (int i = 0; i < SIZE; i++) {
@@ -300,7 +323,7 @@ void mirror() {
                 newImage[i][j] = image[i][255 - j];
             }
         }
-        saveImage();
+        saveGSImage();
     }
     if (n == 3) {
         for (int i = 0; i < SIZE / 2; i++) {
@@ -313,7 +336,7 @@ void mirror() {
                 newImage[i][j] = image[255 - i][j];
             }
         }
-        saveImage();
+        saveGSImage();
     }
     if (n == 4) {
         for (int i = SIZE / 2; i < SIZE; i++) {
@@ -326,14 +349,15 @@ void mirror() {
                 newImage[i][j] = image[255 - i][j];
             }
         }
-        saveImage();
+        saveGSImage();
     }
 }
 
 // SHUFFLE Image #B
 
 // BLUR Image #C
-void blur() {
+void GSblur() {
+    readGSImage();
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             newImage[i][j] = (image[i - 2][j - 2] + image[i - 2][j - 1] + image[i - 2][j] + image[i - 2][j + 1] +
@@ -347,15 +371,33 @@ void blur() {
                               image[i + 2][j + 2]) / 25;
         }
     }
-    saveImage();
+    saveGSImage();
 }
 
-// Load Image
-void loadImage() {
-    char operation;
+//------------------------------RGB BITMAP FILTERS------------------------------//
+void readRGBImage() {
+    char RGBImageFileName[100];
+    cout << "Enter RGB Image Source File Name:";
+    cin >> RGBImageFileName;
+    strcat(RGBImageFileName, ".bmp");
+    readRGBBMP(RGBImageFileName, RGBImage);
+}
+
+void saveRGBImage() {
     char imageFileName[100];
-    cout << "Hello, User Welcome To Our Simple PhotoShop Application"
-            "\nChoose The Method You Want:"
+
+    // Get gray scale image target file name
+    cout << "Enter The Target Image File Name:";
+    cin >> imageFileName;
+
+    // Add to it .bmp extension and load image
+    strcat(imageFileName, ".bmp");
+    writeRGBBMP(imageFileName, newRGBImage);
+}
+
+void loadRGBImageMenu() {
+    char operation;
+    cout << "\nChoose The Method You Want:"
             "\n1.Black & White Filter"
             "\n2.Invert Filter"
             "\n3.Merge Filter"
@@ -371,25 +413,20 @@ void loadImage() {
             "\n0.Exit"
             "\nOperation No.";
     cin >> operation;
-    cout << "Enter The Source Image File Name:";
-    cin >> imageFileName;
-    strcat(imageFileName, ".bmp");
-    readGSBMP(imageFileName, image);
-
     if (operation == '1') {
-        blackAndWhite();
+        GSblackAndWhite();
     }
     if (operation == '2') {
-        invertImage();
+        GSinvertImage();
     }
     if (operation == '3') {
-        merge();
+        GSmerge();
     }
     if (operation == '4') {
-        flip();
+        GSflip();
     }
     if (operation == '5') {
-        darkenAndLighten();
+        GSdarkenAndLighten();
     }
     if (operation == '6') {
         int n;
@@ -400,47 +437,138 @@ void loadImage() {
                 "Operation No.";
         cin >> n;
         if (n == 1)
-            rotate90();
+            GSrotate90();
         else if (n == 2)
-            rotate180();
+            GSrotate180();
         else if (n == 3)
-            rotate270();
+            GSrotate270();
         else if (n == 4) {
-            // Pass
+            cout << "It Remains The Same Image";
         }
 
     }
     if (operation == '7') { // DETECT EDGES
-        detectEdges();
+        GSdetectEdges();
     }
     if (operation == '8') {
-        enlarge();
+        GSenlarge();
 
     }
     if (operation == '9') { // SHRINK
-        shrink();
+        GSshrink();
     }
     if (operation == 'a') { // MIRROR 1/2
-        mirror();
+        GSmirror();
     }
     if (operation == 'b') { // SHUFFLE
 
     }
     if (operation == 'c') { // BLUR
-        blur();
+        GSblur();
     }
     if (operation == '0') {
-
+        cout << "Thanks For using Our Application";
     }
 }
 
-// Save Image
 
+// Load Image
+void loadGSImageMenu() {
+    char operation;
+    cout << "\nChoose The Method You Want:"
+            "\n1.Black & White Filter"
+            "\n2.Invert Filter"
+            "\n3.Merge Filter"
+            "\n4.Flip Image"
+            "\n5.Darken and Lighten Image"
+            "\n6.Rotate Image"
+            "\n7.Detect Image Edges"
+            "\n8.Enlarge Image"
+            "\n9.Shrink Image"
+            "\na.Mirror 1/2 Image"
+            "\nb.Shuffle Image"
+            "\nc.Blur Image"
+            "\n0.Exit"
+            "\nOperation No.";
+    cin >> operation;
+    if (operation == '1') {
+
+    }
+
+    if (operation == '2') {
+
+    }
+    if (operation == '3') {
+
+    }
+    if (operation == '4') {
+
+    }
+    if (operation == '5') {
+
+    }
+    if (operation == '6') {
+        int n;
+        cout << "1.Rotate 90 Clockwise\n"
+                "2.Rotate 180 Clockwise\n"
+                "3.Rotate 270 Clockwise\n"
+                "4.Rotate 360 Clockwise\n"
+                "Operation No.";
+        cin >> n;
+        if (n == 1) {
+
+        } else if (n == 2) {
+
+        } else if (n == 3) {
+
+        } else if (n == 4) {
+            cout << "It Remains The Same Image";
+        }
+
+    }
+    if (operation == '7') { // DETECT EDGES
+
+    }
+    if (operation == '8') {
+
+
+    }
+    if (operation == '9') { // SHRINK
+
+    }
+    if (operation == 'a') { // MIRROR 1/2
+
+    }
+    if (operation == 'b') { // SHUFFLE
+
+    }
+    if (operation == 'c') { // BLUR
+
+    }
+    if (operation == '0') {
+        cout << "Thanks For using Our Application";
+    }
+}
 
 int main() {
-    loadImage();
-    cout << "\nDone!, Thanks For Using Our Application";
-    return 0;
+    int operation;
+    cout << "Hello, User Welcome To Simple Bitmap Images Editor"
+            "\nChoose Type of Images You Want To Filter"
+            "\n1.Grey Scale Image"
+            "\n2.RGB Image"
+            "\n3.Exit"
+            "\nOperation NO.";
+    cin >> operation;
+    if (operation == 1) {
+        loadGSImageMenu();
+    }
+    else if(operation==2){
+
+    }
+    else if(operation==3){
+        cout<<"Thanks For Using Our Application";
+    }else
+        cout<<"Wrong Operation";
 }
 
 
